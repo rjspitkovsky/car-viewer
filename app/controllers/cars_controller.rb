5523e286f8ece3a4586dabf26ae29bc9aa99276e
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-03 14:18:57 -0400

    add cars/new route and new_car erb view

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,6 +8,24 @@ class CarsController < ApplicationController
       @cars = Car.all
       erb :'cars/cars'
     end
-  end 
+  end
+
+  get '/cars/new' do
+    if !logged_in?
+      redirect to '/login'
+    else
+      erb :'cars/new_car'
+    end
+  end
+
+  post '/cars' do
+    @user = current_user
+    if !params.values.include?("")
+      @car = Car.create(manufacturer: params[:manufacturer], condition: params[:condition], value: params[:value], year: params[:year], user_id: @user.id)
+      redirect to '/cars'
+    else
+      redirect to '/cars/new'
+    end
+  end
 
 end
